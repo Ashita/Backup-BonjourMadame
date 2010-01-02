b8614601512c7a19fcef5abbe00b437b42313251
ashita <damonthenet@gmail.com>
2010-01-02 20:45:11 +0100

    add argument to change the directory

--- bjmadam.rb
+++ bjmadam.rb
@@ -4,10 +4,28 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+#checking console argument
+case ARGV.first
+when "-h", "--help", "/h", "/?"
+  puts "Usage : bjmadam.rb <argument> \n\tdirectory : the directory to save all the Madame\n\t\tdefault : current_directory/BonjourMadame\n\t -h, --help : this screen\nCreated by Ashita"
+  exit
+when /\w/
+  @directory_name = ARGV.first if File.directory? ARGV.first
+when nil
+  puts "No directory specified, select default one : #{Dir.pwd}"
+else
+  p "unknow argument"
+  exit
+end
+#end of console argument
+
+##Functions définition
+#grab the page who contains the Madame
 def madam_page(page_number)
   Nokogiri::HTML(open("#{@url}/#{page_number}"))
 end
 
+#get the number of Madame that you always have
 def number_of_present_madam
   begin
     Dir.chdir @directory_name
@@ -19,12 +37,15 @@ def number_of_present_madam
   Dir.glob('Madame*.jpg').length
 end
 
+##variables définitions
 @directory_name = "BonjourMadame"
 @url = "http://www.bonjourmadame.fr/page"
 @max_madam = Integer(madam_page(1).css('div#pages')[0].text.scan(/\d+/).last)
 @present_madam = Integer(number_of_present_madam)
+##end of variables définitions
 
-def picture_link(page)
+#retrive the url of a Madame's picture
+def picture_url(page)
   page.css('div.photo').children.each do |node|
     if node.name == "a"
       yield node.attributes["href"].value
@@ -34,6 +55,7 @@ def picture_link(page)
   end
 end
 
+#save picture data in a file
 def save_in_file(madam_number, data)
   file_name = "Madame_n_#{madam_number}.jpg"
   begin
@@ -46,22 +68,26 @@ def save_in_file(madam_number, data)
   end
   puts "=> #{file_name} writed succesfully"
 end
+##end of function definitions
 
-madam_wanted = @max_madam - @present_madam
-if madam_wanted > 0
-  puts "We found #{madam_wanted} \"Madame\" to download ! !"
-elsif madam_wanted < 0
-  puts "Error, you have more picture than the website. This script need a clean directory"
-else
-  puts "Hey, calm down ! ! You need to wait for 10am of the day to have a new pict ;)"
-end
+## "main"
+if __FILE__ == $0
+  #check if we have some work ;)
+  madam_wanted = @max_madam - @present_madam
+  if madam_wanted > 0
+    puts "We found #{madam_wanted} \"Madame\" to download ! !"
+  elsif madam_wanted < 0
+    puts "Error, you have more picture than the website. This script need a clean directory"
+  else
+    puts "Hey, calm down ! ! You need to wait for 10am of the day to have a new pict ;)"
+  end
 
-(1..madam_wanted).to_a.reverse.each do |n|
-  madam_number = @max_madam + 1 - n
-  puts "Fetching Madame #{madam_number} on page #{n}"
-  picture_link(madam_page(n)) do |link|
-    save_in_file madam_number, open(link).read
+  #do work
+  (1..madam_wanted).to_a.reverse.each do |n|
+    madam_number = @max_madam + 1 - n
+    puts "Fetching Madame #{madam_number} on page #{n}"
+    picture_url(madam_page(n)) do |link|
+      save_in_file madam_number, open(link).read
+    end
   end
 end
-
-

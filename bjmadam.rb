58e872aec1f50f08dd4b2ea1c5af8ab817947fe8
ashita <damien@risterucci.fr>
2012-09-06 09:37:23 +0200

    Constantize constantes, refactoring code, reduce concurrency on typhoeus, and now get images from tumblr.

--- bjmadam.rb
+++ bjmadam.rb
@@ -12,27 +12,34 @@ require 'awesome_print'
 require 'open-uri'
 
 ##variables definitions
-@directory_name = "BonjourMadame"
-@file_name = "Madame"
-@url = "http://www.bonjourmadame.fr/page"
-##end of variables definitions
+DIRECTORY_NAME = "BonjourMadame"
+FILE_NAME = "Madame"
+URL = "http://www.bonjourmadame.fr/page"
+
+# DIRECTORY_NAME = "BonjourPanda"
+# @file_name = "Panda"
+# URL = "http://www.bonjourpanda.fr/page"
+
+# DIRECTORY_NAME = "BonjourGeek"
+# @file_name = "Geek"
+# URL = "http://bonjourlesgeeks.com/page"
 
 ##Functions définition
 #grab the page who contains the Madame
 def madam_page(page_number)
-  Nokogiri::HTML(open("#{@url}/#{page_number}"))
+  Nokogiri::HTML(open("#{URL}/#{page_number}"))
 end
 
 #get all the Madames that you have already stored
 def present_madam
   begin
-    Dir.chdir @directory_name
+    Dir.chdir DIRECTORY_NAME
   rescue Errno::ENOENT => pouet
-    puts "Doesn't find \"#{@directory_name}\" directory, create it now !"
-    Dir.mkdir @directory_name
-    Dir.chdir @directory_name
+    puts "Doesn't find \"#{DIRECTORY_NAME}\" directory, create it now !"
+    Dir.mkdir DIRECTORY_NAME
+    Dir.chdir DIRECTORY_NAME
   end
-  Dir.glob("#{@file_name}_*.jpg").map! { |file| Integer(file.scan(/\d+/).first) }
+  Dir.glob("#{FILE_NAME}_*.jpg").map! { |file| Integer(file.scan(/\d+/).first) }
 end
 
 #retrive the url of a Madame's picture
@@ -42,6 +49,23 @@ def picture_url(page)
       yield node.attributes["href"].value
     elsif node.name == "img"
       yield node.attributes["src"].value
+    end
+  end
+end
+
+def add_request(hydra, url)
+  request = Typhoeus::Request.new(url, :user_agent => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_1; en-us) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10", :follow_location => true)
+  request.on_complete do |response|
+    yield response
+  end
+  hydra.queue request
+end
+
+def add_request_for_image(hydra, url, madam_number)
+  add_request(hydra, url) do |response|
+    if response.body
+      File.open("#{FILE_NAME}_#{madam_number}.jpg", 'w') {|f| f.write(response.body) }
+      puts "#{FILE_NAME}_#{madam_number}.jpg writted"
     end
   end
 end
@@ -56,21 +80,21 @@ if __FILE__ == $0
     puts "Usage : bjmadam.rb <argument> \n\tdirectory : the directory to save all the Madame\n\t\tdefault : current_directory/BonjourMadame\n\t -h, --help : this screen\nCreated by Ashita"
     exit
   when /\w/
-    @directory_name = ARGV.first if File.directory? ARGV.first
+    DIRECTORY_NAME = ARGV.first if File.directory? ARGV.first
   when nil
     puts "No directory specified, select default one : #{Dir.pwd}"
   else
     puts "unknow argument : #{ARGV.first}"
     exit
   end
-  
+
   #get the number of Madame on line
   @fucking_useless_page = 1
   max_madam = Integer(madam_page(1).css('div#pages')[0].text.scan(/\d+/).last) - @fucking_useless_page
   #and remove to that the picture that you already have
   madam_on_disk = present_madam
   madam_wanted = (1..max_madam).to_a - madam_on_disk
-  
+
   puts "#{madam_on_disk.length} found in this directory" if madam_on_disk.any?
 
   #check if we have some work ;)
@@ -81,29 +105,35 @@ if __FILE__ == $0
   end
 
   #init Typhoeus
-  hydra = Typhoeus::Hydra.new :max_concurrency => 20
+  hydra = Typhoeus::Hydra.new :max_concurrency => 5
 
   #do work
   madam_wanted.each do |n|
     madam_number = Integer(n)
     page = max_madam - madam_number + 1
-    request = Typhoeus::Request.new("#{@url}/#{page}", :user_agent => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_1; en-us) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10", :follow_location => true)
-    request.on_complete do |response|
+    add_request(hydra, "#{URL}/#{page}") do |response|
       puts "Fetching Madame #{n} on page #{page}"
-      Nokogiri::HTML(response.body).css('div.photo').children.each do |node|
-        if image_link = node.name == "a" ? node.attributes["href"].value : node.name == "img" ? node.attributes["src"].value : nil
-          img_request = Typhoeus::Request.new(image_link, :user_agent => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_1; en-us) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10", :follow_location => true)
-          img_request.on_complete do |response|
-            if response.body
-              File.open("#{@file_name}_#{madam_number}.jpg", 'w') {|f| f.write(response.body) }
-              puts "#{@file_name}_#{madam_number}.jpg writted"
+      if response.body
+        Nokogiri::HTML(response.body).css('div.photo').children.each do |node|
+          if node.name == "a"
+            add_request(hydra, node.attributes["href"].value) do |response|
+              if response.body
+                image = Nokogiri::HTML(response.body).css('#image').first
+                image ? add_request_for_image(hydra, image.attributes['src'].value, madam_number) : puts("can't download madam #{madam_number} on page #{page}")
+              else
+                puts "no page on #{madam_number} for page #{page}"
+              end
             end
+            break;
+          elsif image_link = node.name == "img" ? node.attributes["src"].value : nil
+            add_request_for_image(hydra, image_link, madam_number)
+            break;
           end
-          hydra.queue img_request
         end
-      end if response.body
+      else
+        puts "Page #{page} not found"
+      end
     end
-    hydra.queue request
   end
 
   hydra.run
